daa81f506970e34a96f4ae6b5a41c1c1a6093410
Brian Glas <brian.glas@gmail.com>
2026-07-26 10:12:31 -0500

    Menu pause sound

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))] 
 public class PauseMenu : MonoBehaviour
 {
+    public AudioSource _audioSource;
+    public AudioClip _pauseSound;
     public GameObject _pauseMenu;
 	bool _isPaused = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _pauseMenu.SetActive(false);
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -34,6 +38,7 @@ public class PauseMenu : MonoBehaviour
 			Debug.Log("pause");
 			Time.timeScale = 0f;
             _pauseMenu.SetActive(true);
+            PlayPauseSound();
             return true;
 			
 		}
@@ -48,4 +53,10 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
 		SceneManager.LoadScene("Main Menu");
 	}
+
+    public void PlayPauseSound()
+    {
+        if (_audioSource && _pauseSound)
+            _audioSource.PlayOneShot(_pauseSound, 1f);
+    }
 }
